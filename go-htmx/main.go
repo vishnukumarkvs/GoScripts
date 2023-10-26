997f3a83fc9fe951d5ff1c91f96ee0b0649d22f9
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-10-26 09:39:07 +0530

    added htmx cdn

--- go-htmx/main.go
+++ go-htmx/main.go
@@ -26,6 +26,11 @@ func main() {
 		}
 		tmpl.Execute(w,films)
 	}
+	h2 := func (w http.ResponseWriter, r *http.Request){
+		log.Println("HTMX request received")
+		log.Println(r.Header.Get("HX-Request"))
+	} 
 	http.HandleFunc("/", h1)
+	http.HandleFunc("/add-film",h2)
 	log.Fatal(http.ListenAndServe(":8000",nil))
 }
