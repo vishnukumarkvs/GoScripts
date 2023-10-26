45bac96b68ff2d7b190ee7dee1ba844bf27574c7
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-10-26 07:38:33 +0530

    sent custom data to html with help of templates

--- go-htmx/main.go
+++ go-htmx/main.go
@@ -6,12 +6,25 @@ import (
 	"text/template"
 )
 
+type Film struct{
+	Title string
+	Director string
+}
+
 func main() {
 
 	//handler
     h1 := func(w http.ResponseWriter, r *http.Request){
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.Execute(w,nil)
+
+		films := map[string][]Film{
+			"Films": {
+				{Title: "Salaar", Director: "Prashant Neel"},
+				{Title: "Pushpa", Director: "Sukumar"},
+				{Title: "RRR", Director: "Rajamouli"},
+			},
+		}
+		tmpl.Execute(w,films)
 	}
 	http.HandleFunc("/", h1)
 	log.Fatal(http.ListenAndServe(":8000",nil))
