b71f1758a07fc064c4fd680793f030325066b716
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-10-26 07:31:18 +0530

    rendered simple html file using go templates

--- go-htmx/main.go
+++ go-htmx/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
+	"text/template"
 )
 
 func main() {
 
 	//handler
     h1 := func(w http.ResponseWriter, r *http.Request){
-		io.WriteString(w,"Hello World\n")
-		io.WriteString(w, r.Method)
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.Execute(w,nil)
 	}
 	http.HandleFunc("/", h1)
 	log.Fatal(http.ListenAndServe(":8000",nil))
