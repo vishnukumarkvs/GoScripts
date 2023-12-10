6cce15e3dc9bbd739ba47fe7335e0c916b591b3f
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-12-10 20:14:07 +0530

    written basic template of using ctor model using hollywood package of anthdm

--- go-actor-model/go.mod
+++ go-actor-model/go.mod
@@ -1,3 +1,11 @@
 module go-actor-model
 
 go 1.21.3
+
+require github.com/anthdm/hollywood v0.0.0-20231209145738-629e47e6e898
+
+require (
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
--- go-actor-model/main.go
+++ go-actor-model/main.go
@@ -1,18 +1,33 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/anthdm/hollywood/actor"
+)
+
 type User struct {
 	state uint
 }
 
-func (h *User) handleFunc(state int) {
-	h.state = uint(state)
+func newHandler() actor.Receiver{
+	return &User{}
 }
 
-func main() {
-	user1 := User{}
-	for i := 0; i < 10; i++ {
-		go user1.handleFunc(i)
+func (h *User) Receive(c *actor.Context) {
+	switch msg := c.Message().(type) {
+	case actor.Initialized:
+		h.state=10
+		fmt.Println("Handler initiaized, my state: ", h.state)
+	case actor.Started:
+		fmt.Println("Handler started")
+	case actor.Stopped:
+		_ = msg
 	}
 }
 
-// go run --race main.go -> data race occurs
+func main() {
+	e,_:= actor.NewEngine()
+	pid:=e.Spawn(newHandler, "handler")
+	fmt.Println("pid->",pid)
+}
