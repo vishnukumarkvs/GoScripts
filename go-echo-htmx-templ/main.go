33861404f7c53525992d2ea4735684a0b416ddb7
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-12-21 20:48:40 +0530

    chnaged button to form int todo input - best approach

--- go-echo-htmx-templ/main.go
+++ go-echo-htmx-templ/main.go
@@ -11,21 +11,31 @@ import (
 )
 
 func main() {
-    items:= []dto.TableItem{{1,"go learn","done"},{2,"leetcode daily","new"}}
-    e := echo.New()
-    component := templates.Index(items)
-    component.Render(context.Background(), os.Stdout)
-    e.GET("/", func(c echo.Context) error {
-        return component.Render(context.Background(), c.Response().Writer)
-    })
-    e.POST("/add-todo", func(c echo.Context) error {
-        todoText:= c.FormValue("todoText")
-        item := dto.TableItem{len(items)+1,todoText,"new"}
-        items = append(items, item)
+	items := []dto.TableItem{{1, "go learn", "done"}, {2, "leetcode daily", "new"}}
+	e := echo.New()
+	component := templates.Index(items)
+	component.Render(context.Background(), os.Stdout)
+	e.GET("/", func(c echo.Context) error {
+		return component.Render(context.Background(), c.Response().Writer)
+	})
+	e.POST("/add-todo", func(c echo.Context) error {
+		todoText := c.FormValue("todotext")
+		// formValues, _ := c.FormParams()
+		// fmt.Println(len(formValues))
 
-        component := common.Table(items)
-        return component.Render(context.Background(), c.Response().Writer)
-    })
-    e.Static("/css", "css")
-    e.Logger.Fatal(e.Start(":3000"))
+		// // Iterate through the form values and list them
+		// for key, values := range formValues {
+		// 	for _, value := range values {
+		// 		// Print or process each form key-value pair
+		// 		fmt.Printf("Form Key: %s, Form Value: %s\n", key, value)
+		// 	}
+		// }
+		item := dto.TableItem{len(items) + 1, todoText, "new"}
+		items = append(items, item)
+
+		component := common.Table(items)
+		return component.Render(context.Background(), c.Response().Writer)
+	})
+	e.Static("/css", "css")
+	e.Logger.Fatal(e.Start(":3000"))
 }
